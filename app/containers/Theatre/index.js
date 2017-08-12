637ab55135f4299c7ca58e8e7cfb4b7d08c71d4e
bubbles <samerce@gmail.com>
2017-08-11 20:25:41 -0400

    fixed prod

--- app/containers/Theatre/index.js
+++ app/containers/Theatre/index.js
@@ -4,6 +4,9 @@ import {
   CatchLine,
   SweetTalk,
 } from '../../global/styled'
+import {
+  Root,
+} from './styled'
 
 const Background = styled.div`
   background: no-repeat url('https://c1.staticflickr.com/3/2565/4096382636_7e7d9d5545_o.png');
@@ -17,7 +20,7 @@ const Background = styled.div`
 export default class Theatre extends React.Component {
   render() {
     return (
-      <div>
+      <Root>
         <Background />
         <CatchLine style={{margin: '20px'}}>
           from performance art to president
@@ -25,7 +28,7 @@ export default class Theatre extends React.Component {
         <SweetTalk>
           a politically-charged meditation on ideas — on hope, fear, war, education, addiction, god, love, loss, and what it means to be alive today.
         </SweetTalk>
-      </div>
+      </Root>
     )
   }
 }
