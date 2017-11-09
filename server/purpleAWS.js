5afca503c5b8408d15c038f76a717061b532db9f
bubbles <samerce@gmail.com>
2017-11-09 18:14:25 -0500

    fixed crash on aws confg in heroku

--- server/purpleAWS.js
+++ server/purpleAWS.js
@@ -7,8 +7,10 @@ module.export = {
 }
 
 function loadConfig() {
-  let config = require('../aws.config.json')
-  if (!config) {
+  let config;
+  try {
+    config = require('../aws.config.json')
+  } catch (e) {
     config = {
       accessKeyId: process.env.AWS_ACCESS_KEY_ID,
       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
