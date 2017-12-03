5a5a2f77ca49545c74ea2c9bd1b3b70fb369a517
Samer Chahine <samerce@gmail.com>
2017-12-03 12:44:26 -0500

    updated bit-by-bit with new writing

--- app/containers/bitbybit/writing.js
+++ app/containers/bitbybit/writing.js
@@ -1,67 +1,94 @@
 export default `
-man — hip ideals blew a gasket somewhere on interstate 6. it’s high-rise and cement now. there’s a formula. effective as borax on boxelders. dammit, the females need twelve unnatural hours of sunlight and nothing more. else they turn male ganja and cloning the glue is through. destination nowhere. we already arrived, ya see. pious purposes; pioneers without the native buffalo blood, intonement; tiddled shellac of nature’s immovable will that just needs a couple well-maneuvered slow square roots to settle the tab.
-shambala dreamin’ — fraternal flights of dearly chirped n’ chirping ballooned hearts, vibrate your dazzling hue. shh-uhmmm-baahhh-lllaaaa !
-nature
-two beefy boys shame me at the cosmic gym because they can lift infinite. samer shakes me awake, “ricky, ricky. it’s ok.” apparently i’m breathing heavy. the dream takes me under.
-each stop someone dies.
-sleep.
-tribal father digs hole. drops recently crippled son in to die looking up and out.
-“can a living man fall upon a crippled dying man to die also?” one boy asks the father. “yes” he says.
-the healthy boy jumps and lands on the crippled boy to die. they are stacked. looking up. a ceremony. ritual. joy. it may take days. weeks. another jumps and lands. the middle healthy boy fist pounds the air in celebration. power in numbers. they are choosing this. one more jumps in. four boys. lying atop each other. the crippled boy on bottom. above the naked earth. settling in for their dirt nap. rigid belief. a knowing. this must come to pass for all.
-the buddha cesses suffering.
-suicide isn’t a real thing. not like we paint it. it’s not a sin. it’s not anything. it just is. like a potato chip. or ISIS. or grass. or billygoats. or dreams. all is called back. only to return to the paradox of the living.
-sleep.
-the last stop. the crash. a berg takes the rest.
-“is everything $7?” I ask for the last time, holding up a container of greek yogurt.
-“no. that’s…” his eyes water, “five… forty….” suddenly i get it. he’s making it up. it doesn’t matter. none of us are leaving this snowy fortress. this is the end.
-“maybe i can make it last a week” the guy says.
-why did he sell it to him? does he know? does it matter?
-intention and perception are all we have.
-consciousness and life are all we have.
-silver tin man penguin dove pear plunges to unlock the sidewalk… falls (jumps?) to ski the stair rail… “i’m gonna need a medic” he says to incredulousness and laughter. he flies. lands in sitting position, ass-to-rail — 50 feet down.
-dreams are beauty.
-first-person VR where anything is possible.
-a trading post deep in the woods manned by Her. forever stuck in 1926. born in 1900, they invite me to celebrate his twenty-sixth birthday with whiskey shots and hard slaps to the back.
-the flaming bus with the breasted women taking me to safety.
-ethan and a man at the corner
-the red voicemail gak-covered from the gutter with the $1 local carwash to rinse clean.
-him outside the hospital. so broken. so free. 7 feet diameter. legs wrapped in foil. one smile.
-why did i wait to visit?
-why am i so afraid of him? one little forehead tin kiss.
-fifty feet.
-peaches.
-6:45am. water. a patch of sun. one lone boat heading off onto grass lake. a blonde pony tail.
-each time i wake, i remember. i am called back. surrender. sleep. sweat. tears. life clicks. dreams click. fall.
-paradox folds. both side at once. the living. something else.
-math problems. the speed at which one can negotiate numbers. play with them. infinite dances. potential realities conjured by decimal points. this is where choice resides.
-words don’t complete the paradox. feeling does. listening. hearing. the vortex has been pitched.
-emily. the conjured firefly. a green patch of grass. flitters us to safety. a fire prod. she says i’m
-addicted to him. addicted to things. to stories. to themes. to self. to fear. a big aggressive ego that is hungry always.
-see-more sandcastle
-why am i in my car right now? action, movement without thought. deliberation kills the spark. paradoxical ever-after wants speech. i am in a vortex portal with infinite dances on the line of the paradox. i am permanent drugged and drugging. dreams won’t stop. round and round. silence welcomes noise. inside the mitten my geographical presence appears to be. grasslake feels like it.
-i wake from a world of dreams to enter a world of dreams. one where i drive to an old ice cream shop in grass lake for coffee at the orange counter. the lady is breathing and moving. i cannot get my head up straight. breathing into this.
-this morning high. on nothing. on dreams. delirium. momentum. circles. feeling. breath.
-corbu. that night. those hours. her. the stairs. her finger. watching a single family of martians from first encounter through forever. each moment one more degree of separation.
-waves. particles. collision. entanglement. quantums. quarks.
-why amy. why samer. why nick.
-how to protect this. i am permanently drugged and drugging. on it. of it. because of it. big bang. a hit of hash.
-plastic baby dolls with plastic curls in the oversized small faded fleck white barn house window looking down to me, a swan craned and careful in careless elongated leg neck.
-i cannot ask questions. i cannot give answers.
-there’s no room at the tower that does not suffer from issues of space. tin man sie-more sandcastle lives on a puffer cloud outside of time and electricity bills and straw polls and kindergarten shoe-tying tests. a place where when he poops his ninja turtle undergarments they clean themselves with avocado fluff peppered in coffeemate that isn’t full of plastic and poison well that kills not cuz no one can die. only level this and that. sober pancakes and the mooart deep freeze in a looney fringe sugar packet that doesn’t believe in calories or glass sundae bowls. only a head that can’t sit up on its nibble nobby neck, an ever-tilt — cocked and buoyant.
-squash soup wearing cowboy hat small as a pickle only to be big as a house, sweet twinkie — how does a hat go big, small, swell like that? is it his beard? my breathing is scattered, deep, short, bellicose breath. i cannot talk another syllable more from here to then.
-i need a fortune cookie from the skittle bird to pop frankincense for mary who is no longer a virgin but still a virgin in the untouchable crosshair axis of axes where it counts.
-what is decipherable to whom?
-her finger touches my finger. two tips. she scurries under the stairs. grabs at feet. seduces the forest, electric. i come back to her. a white plastic ring of eternity is what i can leave but not what i can offer. pluck a star, put it in his bellybutton for safe keeping. a mouthful of eternity.
-feeling this much, running this far outside the gamut, comes from price and cost that forms in nausea. breathe. always breathe. it’s the one thing. the ocean’s wave. where particle meets wave. where nothing becomes something.
-i can tow the line between the curve and line. eternity in a kale chip. the southeast corner. don’t eat my crisp, but eat it. i need to walk eternity. no rush, ginger.(ly).
-i am an astronaut. i am in space. i am orbit. orbit me. me orbit. you.
-i want to eat my dreams. i want to stuff my face with nonsense that rings true. hark le. an exploding imploding reloading heart breast freckle.
-missy’s grass shack. so many club sandwiches. everyone wants a club. in them. on them.
-my tree man, ant man, yes we can man — play with me in the eternal forrest of gretel’s hans. man. chase ants the size of pizzas and beetles as big as cans of coke without cancer. grow imaginary wheat with me. flicker. the relativity of it. perceive with me. let’s stop time, renegotiate how we tally mark our time. spent. sun breaks her way. 40-foot fractal elephants. lady frogs on stilts hula hooping to pringle flow of purple rain for a prince can only mustn’t but will bring us to worship under thunder skies and ghee-tars made of ivory in golden milks and abdominal flanks strong and tight as a blade of thistle mark. the tax man can’t come for us here. voter laws do not apply. war budget is connect-the-porcupickle fickle fawna in the land of plenty good.
-flow finds itself. it is a crunch of a number. flitter that. swallow this. always a chance for unnecessary rebirth. a second doesn’t go without skin shed.
-pain is an ice cream choice. flavors aren’t predicated. ever tasted salted flow? rock yourself, rock me. sway with it. with me. trees muscle. firm and tone. wind as nature’s dumbbell. threaten the roots. what sticks is eternal. what falls lasts a little more. evermore. nothingmore. edgar was a poe. is. ever shall be. does a raven quote itself? willy boy, the spot won’t come out. nevermind, lady plato as pluto as mimicry winds tortured as a chime game. release into me.
-this isn’t a matter of parliamentary tomato soup. there are no lords or commons here. only pink pantie pulldown and biocentrism of consciousness and life. out beyond physics and presidents and speed limits and centrifugal gravity of reindeer antlers or soda pop bubble smack stoker tax flier miles. fill me up, glinda. it’s time to try defy-ing gravity.
-thomas paine are you safe in my kneecap? shall we swap turns? i’ve rid the ghost. illusory congress. blips. i’m tortured by his gripped reality. letting go is a lifelong pursuit.
-vanished mud. a return through the port-o-potty. clean but for forty-two thousand pieces of it. poop floats in lotus. suddenly i’m hankering for fallen leaves with crunch. disintegration underfoot. a contorted mouth. nothing can be an adjective. gaits won’t fit into strange. labels can’t. only eyeballs. only the witness.
-watercolor jazz, improvised love into bottles of bubbly, holding hearts, vibrated desires of the hearty band of seers — a world willing, a world giving. . . occupying the space beyond words.
-write me a thousand telegrams of a soul’s eternal sunshine but it never rains for those twenty-four hours straight.`
+"don’t you know anyone with hope in their hearts?"
+“no.”
+
+insufficient quench. mother, i thirst to escape life’s drudgery. stories and ideas and people and adventure.
+
+mystic social prophet. biocentric idealist. parnassus is-ness poet punching above my weight.
+genius or fraud as if it matters to the curation.
+i just want an adderall prescription. gangly at 40 isn't as cute.
+
+boldly natural over robust caricaturing. the plight of the artist is the privilege to starve. it is my right. go to the gutter. the game can’t be beat. but the brink is a principled fight. i won’t work. how i earn my daily bread will not be work. a remote martyr’d saint.
+
+a happily sad soul willfully flung against a gray world of gloom.  it won’t be ennobling. privacies of the heart reveal the worst. a life of irrationally budgeted extravagances justified as art. no moral defenses are to be made.
+
+mostly she’s beautiful and open,
+like a flower sprung in june. 
+sometimes she’s closed and ugly,
+but winter's needed for the bloom
+
+
+winter of the mind
+long and merciless
+tread lighter
+
+the eternal quest of the hedonic treadmill
+
+lost. sad. confused. alone. bored. mad. full of pain. and hate. little and afraid. trapped, strangled and desperate. but also free. a joyous happy spirit. rebellious, open and kind. pure love confronting a hard world that is lonely & shadowed, in perpetual pain.
+
+darkness everywhere. joy and light everywhere. the raging range—dark and black and lost. clean and saved and perfect.
+
+possessed, obsessed, thoroughly pursuing an ideal. it never ceases, there is no break, a rich, complete enjoyment—art, war, thought, metaphysical realms. a thorough penetration. an aliveness. a discussion of anything. all tangents and some wine.
+
+ what is poetry in an age of decline ‘cept capturing the fall?
+
+a man who makes potions in life's traveling show.
+
+III. disparate monologues of a thing-seeker. unpolished and sloppy and sometimes don't get all the poop off my ass when i wipe. sometimes all i can do is pity myself and masturbate. cry and drink wine before noon. sometimes i can't get footing. can't shake the shakiness it's all built on. the competition between glass houses that ain’t bulletproof. a giant heart steamrolled by an even bigger ego. a brain that never got enough of the right kind of love in childhood. i know my life has one finish line. and it isn’t a linear path. and i’m already dead if i’m in my head.
+
+IX. desolate insides, a menacing landscape. walk that endless highway towards the horizon kid. the nectar of the gods is in my belly. slept with the virgin mary, the demons are wrestled, buddha in my nostril flare. a demure flirtation. tinker with your universe. but you have to feel it, spirit has to want to come home. this isn’t something you can sell or convince people of; it’s a very solo journey. i didn't know this happy was possible. acceptance and curiosity. i am safe. i am home. i am eternal. giving up the struggle is scary. new truths don’t disrupt your nature’s story. just provides continuity’s lube. the theory of black holes crumble close to the event horizon.
+
+i require so little from humanity. having taken to paving my own luck. the amnesia of living seemed to have misplaced yesterday's true so i've taken to aimless strolling. accommodate or isolate so i choose charcoal sketching in my truck. sitting curbside staring at ants. apparently the pleasant passing of time in the front seat of an SUV feels lurky. irrational and suspicious. my white youth mostly saves me from the hate. well isn't he a queer queer. i'll just hide in my car under the $50k until things start making sense. but this ain't life's last poem cuz my head don't want no oven. bad sylvia.
+
+emotionally lethargic.
+
+impotent against myself— incorrigible. disposable. utterly defenseless.
+
+desolate angel of thoughtful repose, you are alone tonight. a mind muddled by better men. a very lardy pig consumed by a dim, drab waking life from which there isn’t room to recover.
+
+riddled with yards of the world’s intestines - dazed and embittered by her abuses, drunk with her parfumery.
+
+turned on by roses cuz they wilt but mad cuz everything’s always alright.
+
+a homely, prosey girl.
+
+the deplorables are in vogue again. but european residence must wait until after mother dies. the trauma of receiving news of her faulty heart while in hong kong is too much to repeat. i must be a drive away from her now. two hours to sisters and nineteen speedy hours to florida, the two places she now keeps herself. my calls are infrequent and visits far less so, but at least i am in her country.
+
+i'm a crying man, but these are crying times.
+tear-stained crier, unmendably torn yet unabsolved from ‘soul under construction.'
+salivating for salvation in laughter that comes easy. unforced smile of an upturned lip. some clumsy goofing.
+ how did i get this empty? is the sine wave skyward—am i filling up? where is life headed?
+
+i’m early. plight of an eager spirit.
+
+i avoid the world. i'm not used to being avoided. thanks for the experience.
+
+all is something else until it is known as something else. knowledge reveals different truths. 
+
+the mind controls the body because it is the body. sophisticated but predictable clockwork. mental mechanics are still mechanics. mechanical process covers all occupation of space which is to say everything. which is to say nature is a fixed machine doing as it does. our screaming at nature, pulling it about, demanding it operate how we like, is just nature being nature. that is not separate from the process. this thing we call ego is further mechanics of nature. sense receptors noticing stimulus is mechanics of nature. making decisions, feeling scared or loved is mechanics of nature. nature does not change but it does behave according to the stimulus it focuses on. 
+
+nostic, knowing versus agnostic, not knowing.
+
+over-rationalization has pushed out space for beauty. 
+
+beauty exists self-so, further mechanics of nature. 
+
+what happens if tomorrow everyone stops thinking and there is a systemic switch from analysis to observation, to witnessing is-ness. 
+
+and this opened space to freely expressing the mechanics of nature without so much focus on we as humans dictating the process. instead we are filters to project all-things immutable.  
+
+to say there would be more beauty with less survivalist thought is to say there would be more non-egoic demand on what this beauty should or ought look like. 
+
+when does thought become overthought and therefore less beautiful?
+
+and what is thought? how is it separate from knowledge? 
+
+the thinker attributes motives and actions to his rational thought, to the methodical processes leading to what he emotes. and attributes actions of the non-thinker, the human animal, to the mechanical process of nature. a man does what he does because a leaf, table, or billy goat do what they do.
+
+it is a game of language and symbol. existence is a play on what this symbolic language stands for. if you know the color yellow you can more likely draw a sun when asked by your teacher. however, if the word yellow connotes nothing in your brain but a funny sound then you are less likely to complete the task. failure to draw the sun says something about you. about the intelligence of your nature. and how you should fit within society. 
+
+i doubt therefore i think therefore i am versus i think therefore i am versus i feel therefore i am versus i am therefore i am. 
+
+how can will move my index finger? how does the sense perception inside my brain upon seeing a flashing light register sensations throughout my body? and what of the way it makes me feel and how it makes me think? 
+
+all phenomenon is nature focusing itself.`
