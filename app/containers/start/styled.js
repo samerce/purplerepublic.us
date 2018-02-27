bed6ff3ad50a43fc6bf80cd2b76ce3b19082dfb3
bubbles <samerce@gmail.com>
2018-02-26 22:36:37 -0500

    fix bug on android

--- app/containers/start/styled.js
+++ app/containers/start/styled.js
@@ -836,7 +836,7 @@ export const Koki = styled(Text)`
 export const BubbleGrid = styled.div`
   display: flex;
   flex-wrap: wrap;
-  align-content: center;
+  align-content: flex-start;
   width: 100%;
   height: 100%;
   position: absolute;
